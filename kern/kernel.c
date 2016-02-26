0e8d090b38150faed010c8716ff9b1209c9e91b9
nayak16 <aatishn@unix6.andrew.cmu.edu>
2016-02-25 21:17:35 -0500

    Remove kernel.c from .gitignore

---
+++ kern/kernel.c
@@ -0,0 +1,45 @@
+/** @file kernel.c
+ *  @brief An initial kernel.c
+ *
+ *  You should initialize things in kernel_main(),
+ *  and then run stuff.
+ *
+ *  @author Harry Q. Bovik (hqbovik)
+ *  @author Fred Hacker (fhacker)
+ *  @bug No known bugs.
+ */
+
+#include <common_kern.h>
+
+/* libc includes. */
+#include <stdio.h>
+#include <simics.h>                 /* lprintf() */
+
+/* multiboot header file */
+#include <multiboot.h>              /* boot_info */
+
+/* x86 specific includes */
+#include <x86/asm.h>                /* enable_interrupts() */
+
+/** @brief Kernel entrypoint.
+ *  
+ *  This is the entrypoint for the kernel.
+ *
+ * @return Does not return
+ */
+int kernel_main(mbinfo_t *mbinfo, int argc, char **argv, char **envp)
+{
+    /*
+     * When kernel_main() begins, interrupts are DISABLED.
+     * You should delete this comment, and enable them --
+     * when you are ready.
+     */
+
+    lprintf( "Hello from a brand new kernel!" );
+
+    while (1) {
+        continue;
+    }
+
+    return 0;
+}
