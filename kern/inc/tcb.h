4cd5b078e0c0b71d8b3b9f10f42b6b7fd0bd78bc
nayak16 <aatishn@ww109-c10.ww.andrew.cmu.edu>
2016-03-01 19:58:35 -0500

    Add tcb relevant files

--- kern/inc/tcb.h
+++ kern/inc/tcb.h
@@ -1,15 +1,23 @@
-/** @file tcb_t.h
+/** @file pcb_t.h
  *
  */
 
-#ifndef _TCB_T_H_
-#define _TCB_T_H_
+#ifndef _PCB_T_H_
+#define _PCB_T_H_
 
-typedef struct tcb{
-    int tid;
-    int status;
-    int exit_status;
-    // TODO: add register context
-} tcb_t;
+/* page directory include */
+#include <page_directory.h>
+/* frame manager include */
+#include <frame_manager.h>
 
-#endif /* _TCB_T_H_ */
+typedef struct pcb{
+    int pid;
+    page_directory_t pd;
+} pcb_t;
+
+int pcb_init(pcb_t *pcb);
+int pcb_set_running(pcb_t *pcb);
+int pcb_destroy(pcb_t *pcb);
+int pcb_load(pcb_t *pcb, frame_manager_t *fm, const char *filename);
+
+#endif /* _PCB_T_H_ */
---
+++ kern/scheduler/tcb.c
@@ -0,0 +1,97 @@
+/** @file pcb_t.h
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <constants.h>
+#include <pcb.h>
+
+/* set_pdbr */
+#include <special_reg_cntrl.h>
+
+/* get_bytes */
+#include <loader.h>
+#include <mem_section.h>
+#include <virtual_mem_manager.h>
+
+
+/* PAGE_SIZE */
+#include <x86/page.h>
+#include <elf/elf_410.h>
+#include <elf_410.h>
+
+#include <debug.h>
+#include <simics.h>
+
+/* TODO: Should this go here?? */
+#define DIVROUNDUP(num, den) ((num + den-1) / den)
+
+/* TODO: Should this go here?? */
+#define NUM_ELF_SECTIONS 4
+
+#define USER_RO NEW_FLAGS(SET, UNSET, SET, UNSET)
+#define USER_WR NEW_FLAGS(SET, SET, SET, UNSET)
+
+int load_sections(simple_elf_t *elf, pcb_t *pcb, frame_manager_t *fm){
+    mem_section_t secs[NUM_ELF_SECTIONS];
+
+    mem_section_init(&secs[0], elf->e_txtstart, elf->e_txtlen, NULL, USER_RO, USER_RO);
+    mem_section_init(&secs[1], elf->e_datstart, elf->e_datlen, NULL, USER_WR, USER_WR);
+    mem_section_init(&secs[2], elf->e_rodatstart, elf->e_rodatlen, NULL, USER_RO, USER_RO);
+    mem_section_init(&secs[3], elf->e_bssstart, elf->e_bsslen, NULL, USER_WR, USER_WR);
+
+    /* Map all appropriate elf binary sections into user space */
+    if (vmm_user_mem_alloc(&(pcb->pd), fm, secs, NUM_ELF_SECTIONS) < 0) return -1;
+
+    /* Fill in .text section */
+    if (getbytes(elf->e_fname, elf->e_txtoff, elf->e_txtlen,
+                 (char*)elf->e_txtstart) != elf->e_txtlen) return -1;
+
+    /* Fill in .data section */
+    if (getbytes(elf->e_fname, elf->e_datoff, elf->e_datlen,
+                 (char*)elf->e_datstart) != elf->e_datlen) return -1;
+
+    /* Fill in .rodata section */
+    if (getbytes(elf->e_fname, elf->e_rodatoff, elf->e_rodatlen,
+                 (char*)elf->e_rodatstart) != elf->e_rodatlen) return -1;
+
+    /* Clear .bss section */
+    memset((void *)elf->e_bssstart, 0, elf->e_bsslen);
+
+    return 0;
+}
+
+int pcb_init(pcb_t *pcb){
+    if (pcb == NULL) return -1;
+    //TODO: how to choose pid
+    pcb->pid = 1;
+    pd_init(&(pcb->pd));
+    pd_initialize_kernel(&(pcb->pd));
+    return 0;
+}
+
+int pcb_set_running(pcb_t *pcb){
+    if (pcb == NULL) return -1;
+    /* set the current active page table to process's page table */
+    return 0;
+}
+
+int pcb_destroy(pcb_t *pcb){
+    panic("pcb_destroy: Not yet implemented");
+    if (pcb == NULL) return -1;
+    return 0;
+}
+
+int pcb_load(pcb_t *pcb, frame_manager_t *fm, const char *filename){
+    if (pcb == NULL || filename == NULL || fm == NULL) return -1;
+    simple_elf_t elf;
+    if (elf_check_header(filename) != ELF_SUCCESS) return -1;
+    if (elf_load_helper(&elf, filename) != ELF_SUCCESS) return -1;
+    print_elf(&elf);
+    /* for each section, load the appropriate section */
+    if (load_sections(&elf, pcb, fm) < 0) return -1;
+    // set first tcb
+    return -1;
+}
