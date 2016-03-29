b9ffab466423c5788d040ddbc7128f9f98dd7bef
nayak16 <nayak16@github.com>
2016-03-28 20:03:58 -0400

    Sleep syscalls now yields to another thread

--- kern/kernel.c
+++ kern/kernel.c
@@ -121,7 +121,7 @@ int kernel_main(mbinfo_t *mbinfo, int argc, char **argv, char **envp)
 
     /* Load in actual work pcb */
     set_pdbr((uint32_t) pd_get_base_addr(&work_pcb.pd));
-    pcb_load_prog(&work_pcb, "test_readline", 0, NULL);
+    pcb_load_prog(&work_pcb, "test_sleep", 0, NULL);
 
     /* Add work pcb into scheduler */
     scheduler_add_process(&sched, work_pcb, NULL);
