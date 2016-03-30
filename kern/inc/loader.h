cff9956f76864bd3f4a370e7f888ad9c45e2770a
nayak16 <nayak16@github.com>
2016-03-30 17:46:23 -0400

    Make user stack 2 PAGE_SIZEs

--- kern/inc/loader.h
+++ kern/inc/loader.h
@@ -16,9 +16,9 @@
 
 #define USER_STACK_TOP 0xfffffff0
 
-#define USER_STACK_BOTTOM 0xfffff000
+#define USER_STACK_BOTTOM 0xffffe000
 
-#define USER_STACK_SIZE PAGE_SIZE
+#define USER_STACK_SIZE (PAGE_SIZE*2)
 
 /* --- Prototypes --- */
 
