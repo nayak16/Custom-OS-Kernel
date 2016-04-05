3e23f92f2704ba8a8c3d9a31812d741dd29dfc0c
chrisjwei <cjwei@andrew.cmu.edu>
2016-04-04 20:15:45 -0400

    autostack and swexn done

--- kern/handlers/exception_handlers.c
+++ kern/handlers/exception_handlers.c
@@ -28,14 +28,14 @@ int ureg_from_stack(ureg_t *ureg, uint32_t cause, uint32_t *stack){
     if (stack == NULL) return -1;
     ureg->cause = cause;
     ureg->cr2 = get_cr2();
-    memcpy(&(ureg->ds), &(stack[DS_IDX]), REGS_SIZE * sizeof(unsigned int));
+    memcpy(&(ureg->ds), stack, REGS_SIZE * sizeof(unsigned int));
     return 0;
 }
 
 int swexn_execute(uint32_t cause, uint32_t *stack){
     /* get the current context */
-    ureg_t *ureg = malloc(sizeof(ureg_t));
-    if (ureg_from_stack(ureg, cause, stack) < 0) return -1;
+    ureg_t ureg;
+    if (ureg_from_stack(&ureg, cause, stack) < 0) return -1;
     /* get the current tcb */
     tcb_t *cur_tcb;
     if (scheduler_get_current_tcb(&sched, &cur_tcb) < 0) return -2;
@@ -47,13 +47,15 @@ int swexn_execute(uint32_t cause, uint32_t *stack){
     void *arg = NULL;
     if (tcb_deregister_swexn_handler(cur_tcb, &esp3, &eip, &arg) < 0)
         return -3;
-
     /* check if we should execute the previously deregistered handler */
     if (esp3 != NULL && eip != NULL){
-        ((uint32_t *)esp3)[-1] = (uint32_t)(ureg);// 2nd arg
-        ((uint32_t *)esp3)[-2] = (uint32_t)arg;// 1st arg
-        ((uint32_t *)esp3)[-3] = (uint32_t)(NULL); // bogus return addr
-        stack[ESP_IDX] = (uint32_t)(&(((uint32_t *)esp3)[-3]));
+        ureg_t *ureg_ptr = &(((ureg_t *)esp3)[-1]);
+        *ureg_ptr = ureg;
+        uint32_t *arg_ptr = (uint32_t *)(ureg_ptr) - 1;
+        *arg_ptr = (uint32_t)(ureg_ptr);
+        *(arg_ptr-1) = (uint32_t)arg;
+        *(arg_ptr-2) = (uint32_t)(NULL);
+        stack[ESP_IDX] = (uint32_t)(arg_ptr-2);
         stack[SS_IDX] = (uint32_t)SEGSEL_USER_DS;
         stack[EFLAGS_IDX] = (uint32_t)get_user_eflags();
         stack[CS_IDX] = (uint32_t)SEGSEL_USER_CS;
@@ -65,10 +67,10 @@ int swexn_execute(uint32_t cause, uint32_t *stack){
 }
 
 void page_fault_c_handler(uint32_t *stack){
-    lprintf("Segmentation Fault");
     /* attempt to execute swexn */
     if (swexn_execute(SWEXN_CAUSE_PAGEFAULT, stack) == 0)
         return;
+    lprintf("Segmentation Fault");
     thr_set_status(-2);
     thr_vanish();
 }
--- user/libautostack/autostack.c
+++ user/libautostack/autostack.c
@@ -1,9 +1,97 @@
-/* If you want to use assembly language instead of C,
- * delete this autostack.c and provide an autostack.S
- * instead.
+/** @file autostack.c
+ *  @brief Contains implementation for autostack growth handler
+ *
+ *  For legacy single-threaded programs, this registers an exception handler
+ *  that overwrites the default exception handler and looks for page faults.
+ *  This allows us to extend the stack space by essentially doubling the
+ *  amount of stack allocated with calls to newpage. We chose this method of
+ *  increasing stack space as it allows us to quickly resolve page faults
+ *  with a minimal calls to new_pages. In the case that the exception is not
+ *  a pagefault or not related to stack growth (such as a dereference of NULL)
+ *  then our custom exception handler will not be reinstalled and the program
+ *  will resume at the faulting address thus causing the default handler to be
+ *  called.
+ *
+ *  Another interesting design choice is to malloc the exception stack, which
+ *  is given a stack size of 1 page which is plenty. The reasoning behind this
+ *  design decision is that it would be incredibly messy and hard to either
+ *  shift the stack down by 1 page to make room for an exception stack, or
+ *  fragmenting the parent thread's stack by sticking it directly underneath
+ *  the current stack pointeer. The exception stack will be freed in thr_init.
+ *  In the off chance thr_init is never called, we will have memory leaks:
+ *  which is not okay, but I guess it happens.
+ *
+ *  @author Christopher Wei (cjwei), Aatish Nayak (aatishn)
+ *  @bug No known bugs
  */
 
-void
-install_autostack(void *stack_high, void *stack_low)
-{
+
+#include <syscall.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <stdint.h>
+
+#include <simics.h>
+#include "contracts.h"
+
+/** @brief External address to the top of the user space stack */
+extern void *STACK_TOP;
+/** @brief External address to the bottom of the user space stack */
+extern void *STACK_BOTTOM;
+
+/** @brief Pointer to the top of the exception stack */
+void *exception_stack;
+
+/** @brief A custom page fault handler that increases stack space on
+ *         page fault related exceptions
+ *  @param args The arguments passed in to page_fault_handler
+ *  @param ureg The context saved before fault occured
+ *  @return Void
+ */
+void page_fault_handler(void* args, ureg_t *ureg) {
+    if (ureg->cause == SWEXN_CAUSE_PAGEFAULT && (void *)ureg->cr2 != NULL) {
+        /* ensure that args.stack_high is never lower than args.stack_low */
+        ASSERT((uint32_t)STACK_TOP >= (uint32_t)STACK_BOTTOM);
+
+        int curr_stack_size =
+            ((uint32_t)STACK_TOP - (uint32_t)STACK_BOTTOM);
+        uint32_t aligned_stack_size =
+            (uint32_t)((((curr_stack_size-1) / PAGE_SIZE) + 1) * PAGE_SIZE);
+        void *new_stack_low =
+            (void *)((uint32_t)STACK_BOTTOM - aligned_stack_size);
+
+        if (new_pages(new_stack_low, aligned_stack_size) < 0)
+            panic("Could not extend stack!");
+
+        STACK_BOTTOM = new_stack_low;
+
+        /* only re-register the handler if we catch a page fault
+         * after registering handler, restore context */
+        if (swexn((void*) exception_stack, page_fault_handler,
+                  (void*) args, ureg) < 0){
+            MAGIC_BREAK;
+            panic("Couldn't register autostack handler after page fault");
+        }
+        /* SHOULD NEVER GET HERE */
+        panic("Should never get here");
+    }
+    /* restore context, do not register another handler */
+    swexn(NULL,NULL, args, ureg);
+}
+
+/** @brief Sets up automatic stack growth handler
+ *  @param stack_high The top of the currently allocated stack
+ *  @param stack_low The bottom of the currently allocated stack
+ *  @return Void
+ */
+void install_autostack(void *stack_high, void *stack_low) {
+    /* update the global variables */
+    STACK_TOP = stack_high;
+    STACK_BOTTOM = stack_low;
+    /* allocate a exception stack in the heap */
+    exception_stack = _malloc(sizeof(void*) * PAGE_SIZE);
+    /* install custom exception handler */
+    if (swexn(exception_stack, page_fault_handler, NULL, NULL) < 0)
+        panic("Couldn't register autostack handler");
 }
