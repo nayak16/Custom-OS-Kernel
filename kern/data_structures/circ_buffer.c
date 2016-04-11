7483c76e5821bed580eeb0b56ab474d590c8ce20
chrisjwei <cjwei@andrew.cmu.edu>
2016-04-11 14:50:01 -0400

    circ_buffer docs

--- kern/data_structures/circ_buffer.c
+++ kern/data_structures/circ_buffer.c
@@ -8,6 +8,12 @@
 #include <stdlib.h>
 
 /* Implementation */
+
+/** @brief Initializes a circular buffer of size len
+ *  @param circ_buf The circular buffer
+ *  @param len The maximum length to cap it at
+ *  @return 0 on success, negative integer code on failure
+ */
 int circ_buf_init(circ_buf_t *circ_buf, uint32_t len){
     if (circ_buf == NULL || len == 0) return -1;
     circ_buf->i_r = 0;
@@ -19,12 +25,21 @@ int circ_buf_init(circ_buf_t *circ_buf, uint32_t len){
     return 0;
 }
 
+/** @brief Destroys a circular buffer
+ *  @param circ_buf The circular buffer
+ *  @return Void
+ */
 void circ_buf_destroy(circ_buf_t *circ_buf){
     if (circ_buf == NULL) return;
     free((void *)circ_buf->buf);
     return;
 }
 
+/** @brief Writes a value to the circular buffer
+ *  @param circ_buf The circular buffer
+ *  @param v The value to write
+ *  @return 0 on success, negative integer code on failure
+ */
 int circ_buf_write(circ_buf_t *circ_buf, void *v){
     if (circ_buf == NULL) return -1;
     /* move i_r to next spot*/
@@ -44,6 +59,11 @@ int circ_buf_write(circ_buf_t *circ_buf, void *v){
     }
 }
 
+/** @brief Reads a value out of the circular buffer
+ *  @param circ_buf The circular buffer
+ *  @param v The value to read into
+ *  @return 0 on success, negative integer code on failure
+ */
 int circ_buf_read(circ_buf_t *circ_buf, void **v){
     if (circ_buf == NULL) return -1;
     if (circ_buf->n == 0) return -2;
@@ -56,6 +76,10 @@ int circ_buf_read(circ_buf_t *circ_buf, void **v){
     return 0;
 }
 
+/** @brief Removes the most recently written value in the circular buffer
+ *  @param circ_buf The circular buffer
+ *  @return 0 on success, negative integer code on failure
+ */
 int circ_buf_delete_front(circ_buf_t *circ_buf){
     if (circ_buf == NULL) return -1;
     if (circ_buf->n == 0) return -2;
@@ -64,12 +88,21 @@ int circ_buf_delete_front(circ_buf_t *circ_buf){
     return 0;
 }
 
+/** @brief Returns the number of values in the circular buffer
+ *  @param circ_buf The circular buffer
+ *  @return 0 on success, negative integer code on failure
+ */
 int circ_buf_count(circ_buf_t *circ_buf, uint32_t *count){
     if (circ_buf == NULL) return -1;
     *count = circ_buf->n;
     return 0;
 }
 
+/** @brief Gets the maximum capacity of a circular buffer
+ *  @param circ_buf The circular buffer
+ *  @param len The address to store the result in
+ *  @return 0 on success, negative integer code on failure
+ */
 int circ_buf_size(circ_buf_t *circ_buf, uint32_t *len){
     if (circ_buf == NULL || len == NULL) return -1;
     *len = circ_buf->len;
