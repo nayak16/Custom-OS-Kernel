28b1d5a77b52793160c35d39640289259f560c81
chrisjwei <cjwei@andrew.cmu.edu>
2016-04-13 13:55:04 -0400

    handlers docced

--- kern/handlers/peripheral_handlers.c
+++ kern/handlers/peripheral_handlers.c
@@ -1,3 +1,10 @@
+/** @file peripheral_handlers.c
+ *  @brief Implements handlers for peripheral hardware
+ *  @author Christopher Wei (cjwei)
+ *  @author Aatish Nayak (aatishn)
+ *  @bug No known bugs
+ */
+
 #include <simics.h>
 #include <x86/asm.h>
 #include <x86/interrupt_defines.h>
@@ -12,15 +19,16 @@
 #include <x86/keyhelp.h>
 #include <circ_buffer.h>
 
-
+/** @brief Implements the timer handler
+ *  @param old_esp The stack pointer of the thread that was just running
+ *  @return The stack pointer of the thread that was selected to run by the
+ *  scheduler
+ */
 uint32_t c_timer_handler(uint32_t old_esp) {
     sched.num_ticks++;
 
     /* Wake up any sleeping threads */
-    if (scheduler_wakeup(&sched) < 0){
-        lprintf("Error waking up threads");
-        MAGIC_BREAK;
-    }
+    scheduler_wakeup(&sched);
 
     /* Context switch into scheduler determined tcb,
      * possibly into a thread that was just woken up */
@@ -30,6 +38,12 @@ uint32_t c_timer_handler(uint32_t old_esp) {
     return new_esp;
 }
 
+/** @brief Implements the keyboard handler
+ *
+ *  Reads a scancode from the keyboard port and processes it into a character
+ *
+ *  @return Void
+ */
 void c_keyboard_handler(){
     unsigned char aug_char = inb(KEYBOARD_PORT);
     outb(INT_CTL_PORT, INT_ACK_CURRENT);
